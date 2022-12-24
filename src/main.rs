117f947f7aaaae7bec13d71b90a8002fbabd192c
Aschull <andrews.fernandesx@gmail.com>
2022-12-24 11:57:34 -0300

    Testes com .mount()

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,17 @@ extern crate rocket;
 
 #[get("/teste")]
 fn index() -> &'static str {
-    return "Hello, world!";
+    return "Hello, Wworld!";
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-    .mount("/", routes![index])
-    .mount("/", routes![rs::ola])
+    .mount("/", routes![index, rs::ola])
+
+    //  se mudar a base ai deve se usar um novo "mount"
+    // .mount("/teste", routes![rs::ola])
+
+    // funciona incluir varias rotas na mesma lista assim:
+    //.mount("/", routes![index, rs::ola, ...etc])
 }
